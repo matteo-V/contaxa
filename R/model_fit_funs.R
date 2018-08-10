ba055514411cc36b6a2813c17c0205f07ab44716
Matteo Vaiente <Matteo.a.v@gmail.com>
2018-08-10 02:39:19 -0400

    remove prov location vars

--- R/model_fit_funs.R
+++ R/model_fit_funs.R
@@ -90,7 +90,6 @@ get_clean_illness_covariates <- function(dat){
          live_state_prov,
          dedicated_location_for_waste,
          work_location_prov,
-         dedicated_location_for_waste,
          #had_symptoms_in_last_year_other_people,
          -matches("_notes"), #remove notes cols,
          matches("meat"),
@@ -106,8 +105,6 @@ get_clean_illness_covariates <- function(dat){
   #mutate character vectors to factors where appropriate
   mutate_at(.vars = vars(gender,
                       length_lived,
-                      live_state_prov,
-                      work_location_prov,
                       highest_education,
                       highest_education_mother),
             .funs = funs(factor)
@@ -193,20 +190,16 @@ get_clean_illness_covariates <- function(dat){
   ed2_expand_wide(highest_education_mother) %>%
   ed2_expand_wide(gender) %>%
   ed2_expand_wide(length_lived) %>%
-  ed2_expand_wide(live_state_prov) %>%
-  ed2_expand_wide(work_location_prov) %>%
   #drop counting and other expanded factor variables
   select(-n,
          -occupation,
          -highest_education,
          -highest_education_mother,
          -gender,
-         -length_lived,
-         -live_state_prov,
-         -work_location_prov
-         ) %>%
+         -length_lived
+         )
   #scale and center numeric variables to mean 0 and sd 1
-  mutate_if(is.numeric, scale)
+  #mutate_if(is.numeric, scale())
 
 }
 
@@ -456,39 +449,32 @@ get_illness_analysis_dat <- function(dat,
 #' @importFrom  Matrix rankMatrix
 remove_colinear_columns <- function(dat){
   cat('WARNING: This function requires large amonuts of compute power.')
-  outcome <- dat[,1]
-  outcome_var <- colnames(dat)[1]
+  #outcome <- dat[,1]
+  #outcome_var <- colnames(dat)[1]
   # TODO: test rank reduce algo on full data matrix (with interactions)
   # TODO: allow users to specify interactions
-  # form <- as.formula(paste0(outcome_var, ' ~ ', ' + ', '.', ' + ', '(.)^2'))
-  # model_matrix <- model.matrix(form, data = dat)
-  model_matrix <- data.matrix(dat[,-1])
-  mat_rank <- rankMatrix(model_matrix[,-1]) #rank of matrix without outcome variable
+  #form <- as.formula(paste0(outcome_var, ' ~ ', ' + ', '.', ' + ', '(.)^2'))
+  #model_matrix <- model.matrix(form, data = dat)
+  #remove duplicates
+  model_matrix <- remove_duplicate_columns(dat)
+  mat_rank <- rankMatrix(model_matrix) #rank of matrix without outcome variable
   cat('\nMatrix Rank: ', mat_rank)
   var_names <- colnames(model_matrix) #remove outcome varaible name
 
-  for(ii in 1:length(var_names)){
+  for(ii in length(var_names):1){
     tmp_matrix <- model_matrix[,-ii]
     tmp_rank <- rankMatrix(tmp_matrix)
     if(tmp_rank == mat_rank){ #if rank unchanged with removal
-      model_matrix <- model_matrix[,-ii]
+      model_matrix <- tmp_matrix
     }
     else{
       next; #skip to next iteration
     }
   }
-  for(jj in 1:ncol(model_matrix)){
 
-    tmp_matrix <- model_matrix[,-jj]
-    tmp_rank <- rankMatrix(tmp_matrix)
-    if(tmp_rank == mat_rank){
-      model_matrix <-  model_matrix[,-jj]
-      }
-
-  }
-  final_matrix <- cbind(outcome, model_matrix)
-  removed_ii <- (!(colnames(dat[,-1]) %in% colnames(model_matrix)))
-  removed_vars <- colnames(dat[,-1])[removed_ii]
+  final_matrix <- cbind(model_matrix)
+  removed_ii <- (!(colnames(dat) %in% colnames(model_matrix)))
+  removed_vars <- colnames(dat)[removed_ii]
   return(list(removed_vars = removed_vars, model_data_matrix = final_matrix))
 }
 
@@ -497,7 +483,7 @@ remove_colinear_columns <- function(dat){
 #' @return matrix with duplicated columns removed for lasso and inference
 #' @author matteo-V
 #' @importFrom base apply duplicated.default any
-#' @export
+#' @noRd
 remove_duplicate_columns <- function(model_matrix){
   model_matrix <- model_matrix[,
                                !duplicated.default(
@@ -514,58 +500,208 @@ remove_duplicate_columns <- function(model_matrix){
   return(model_matrix)
 }
 # ############################# LASSO Regularized Logistic Regression ##########################
-#
-# #illness_model_dat_split <- train_test_split(illness_model_dat)
-# #get optimal lambda
-# lambda.fit <- cv.glmnet(x = ili_x_matrix ,
-#                        y =  ili_model_dat[,1] ,
-#                        nfolds = , #5-fold CV
-#                        family = 'binomial',
-#                        #lambda = sapply(1:20, function(x) 2^(-x)),
-#                        alpha = 1) #LASSO regularize
-# plot(lambda.fit)
-# cat('Minimum value of lambda by 5-fold CV:', lambda.fit$lambda.min)
-# lam <- lambda.fit$lambda.min
-# n <- length(ili_model_dat[,1])
 
-#fit glmnet with optimal lambda on training data
-# lasso.fit <- glmnet(x = ili_x_matrix,
-#                     y = ili_model_dat[,1],
-#                     family = 'binomial',
-#                     lambda = lam , #use CV'd lambda.min
-#                     alpha = 1,  #LASSO
-#                     standardize = T,
-#                     thresh = 1e-25)
-# #summarize model
-# lasso.fit
-# #get non-zero coefficients
-# exp( coef(lasso.fit)[which( coef(lasso.fit) != 0),])
-# #get class predictions
-# lasso.pred <- predict(lasso.fit,
-#                       newx = ili_x_matrix,
-#                       type = 'class')
-# lasso.pred
-#
-# #get misclassification rate
-# lasso.conf.matrix <- table(ili_model_dat[,1], as.factor(lasso.pred))
-# lasso.conf.matrix
-# 1 - ( sum(diag(lasso.conf.matrix)) / sum(lasso.conf.matrix))
-#
-#
-# #glmnet multiplies the first term by a factor of 1/n
-# betas = coef(lasso.fit,
-#              s = lam/n,
-#              exact = T,
-#              x = ili_x_matrix ,
-#              y = as.factor( ili_model_dat[,1] ))[-1] #[-1] removes the intercept
-#
-# #perform inference for glmnet (LASSO) model
-# lasso.inference <-
-#   fixedLassoInf(x = ili_x_matrix,
-#                 y = as.logical( ili_model_dat[,1] )*1 ,
-#                 beta = betas,
-#                 lambda = lam
-#   )
+#' perform cross-validation to get optimal elastic net parameter
+#' @param model_matrix model matrix (features, no response) to be used for logistic regression
+#' @param reponse vector of response values (binary) for logisitic regression
+#' @param cv_folds number of folds for cross_validation
+#' @param verbose logical indicating wheter to print lambda to std output
+#' @return lambda which minimizes the binomial deviance
+#' @author matteo-V
+#' @importFrom glmnet cv.glmnet plot.cv.glmnet
+#' @noRd
+cv_elasticnet_param <- function(alpha_vals, model_matrix, response, cv_folds){
+  cat("Performing alpha cross validation\n")
+  res <- list()
+  for(alpha in alpha_vals){
+    lambda.fit <- cv.glmnet(x = model_matrix,
+                            y = response ,
+                            nfolds = cv_folds, #5-fold CV
+                            family = 'binomial',
+                            alpha = alpha)
+    res[[as.character(alpha)]] <- lambda.fit$cvm
+  }
+  res <- stack(res)
+  #get min alpha
+  as.numeric(as.character(res$ind[which.min(res$values)]))
 
+}
+
+#' perform cross-validation to get optimal lambda (shrinkage) parameter
+#'
+#' @param model_matrix model matrix (features, no response) to be used for logistic regression
+#' @param reponse vector of response values (binary) for logisitic regression
+#' @param cv_folds number of folds for cross_validation
+#' @param verbose logical indicating wheter to print lambda to std output
+#' @return lambda which minimizes the binomial deviance
+#' @author matteo-V
+#' @importFrom glmnet cv.glmnet plot.cv.glmnet
+#' @noRd
+cv_shrinkage_param <- function(model_matrix, response, cv_folds=5, verbose = F, ...){
+  # best_alpha <- cv_elasticnet_param(seq(0.05,1,by=0.05), model_matrix, response, cv_folds)
+  # cat("Best alpha by cross validation:", best_alpha)
+  lambda.fit <- cv.glmnet(x = model_matrix,
+                          y = response ,
+                          nfolds = cv_folds, #5-fold CV
+                          family = 'binomial',
+                          lambda = 10^-(seq(1, 2, by=0.005)),
+                          alpha = 1) #LASSO regularize
+  #plot bias variance tradeoff curve
+  plot(lambda.fit)
+  #print value of minimum lambda
+  if(verbose){
+    cat('Minimum value of lambda by 5-fold CV:', lambda.fit$lambda.min)
+  }
+  #select minimum lam
+  lam <- lambda.fit$lambda.min
+  return(lam)
+}
+
+#' fit glmnet with optimal lambda on training data
+#'
+#' @param model_matrix model matrix of features (no response column) for logisitc regression
+#' @param response response vector (binary)
+#' @return fitted glmnet object
+#' @author matteo-V
+#' @importFrom glmnet glmnet
+#' @export
+fit_lasso_model <- function(model_matrix, response){
+  #get optimal lambda
+  lam <- cv_shrinkage_param(model_matrix, response)
+  #fir lasso model
+  lasso.fit <- glmnet(x = model_matrix,
+                      y = response, #fit using logical labels
+                      family = 'binomial',
+                      lambda = lam, #use CV'd lambda.min
+                      alpha = 1,  #LASSO
+                      standardize = T,
+                      thresh = 1e-30)
+  return(lasso.fit)
+}
+
+
+#' Computes misclassification rate of a fitted lasso model
+#' @param lasso_fit from fit_lasso_model function
+#' @param model_matrix heldout model matrix of features
+#' @param response response (y) vector
+#' @author matteo-V
+#' @importFrom glmnet predict.glmnet
+#' @export
+compute_lasso_performance <- function(lasso_fit, model_matrix, response){
+  #get class predictions
+  pred <- predict(lasso_fit,
+                  newx = model_matrix,
+                  type = 'class')
+
+  pred <- as.logical(pred)
+  #get misclassification rate
+  lasso.conf.matrix <- table(pred, response)
+  cat('Confusion Matrix:\n')
+  print(lasso.conf.matrix)
+  #get misclassification rate
+  misclass_rate <- 1 - ( sum(diag(lasso.conf.matrix)) / sum(lasso.conf.matrix))
+  cat("\nMisclassification rate:", misclass_rate)
+  #compute sensitivity
+  sens <- lasso.conf.matrix[2,2] / (lasso.conf.matrix[2,2] + lasso.conf.matrix[1,2])
+  cat("\nFalse negative rate:", 1 - sens)
+  #compute specificity
+  specif <- lasso.conf.matrix[1,1] / (lasso.conf.matrix[1,1] + lasso.conf.matrix[2,1])
+  cat("\nFalse positive rate:", 1 - specif)
+}
+
+#' Perform Inference on fitted lasso model
+#'
+#' @param lasso_fit from fit_lasso_model
+#' @param model_matrix model matrix of features
+#' @param response vector of response values
+#' @return formatted LASSO inference table (variable, mean odds ration, 95% ci, p-values)
+#' @author matteo-V
+#' @importFrom stats coef
+#' @importFrom selectiveInference fixedLassoInf
+#' @export
+perform_lasso_inference <- function(lasso_fit, model_matrix, response){
+  cat("WARNING: This function may take a while to execute fully")
+  #get lambda and N
+  lam <- lasso_fit$lambda
+  n <- length(response)
+  #get betas
+  betas <- coef(lasso_fit,
+                s = lam/n, #glmnet multiplies the first term by a factor of 1/n
+                exact = T,
+                x = model_matrix,
+                y = as.factor( response )) #[-1] removes the intercept
+
+  #perform inference for glmnet (LASSO) model
+  lasso.inference <-
+    fixedLassoInf(x = model_matrix,
+                  y = as.logical( response )*1 ,
+                  beta = betas,
+                  lambda = lam,
+                  family = 'binomial',
+                  gridrange = c(1e-6, 1e6))
+
+  #variables names for output
+  var_names <- names(lasso.inference$vars)
+
+  #mean effect sizes
+  effects <- lasso.inference$coef0
+
+  #lower CI bound
+  lower_ci <- lasso.inference$ci[,1]
+  upper_ci <- lasso.inference$ci[,2]
+
+  #pvalues
+  pvalues <- lasso.inference$pv
+
+  #create table
+  lasso_inference_table <- data_frame(var_names = var_names,
+                                      effect_size=effects,
+                                      lower=lower_ci,
+                                      upper=upper_ci,
+                                      pvalues=pvalues)
+
+  #annotate significant values
+  lasso_inference_table$significant <- (lasso_inference_table$pvalues < 0.05)
+
+  #convert to odds ratios
+  lasso_inference_table <-
+    lasso_inference_table %>%
+    mutate_at(.vars = vars(effect_size, lower, upper), .funs = funs(exp)) %>%
+    mutate(var_names = str_replace_all(var_names, '_', ' '))
+
+  return(lasso_inference_table)
+
+}
+
+
+#' plot output from perform_lasso_inference
+#'
+#' @param lasso_inference_table from perform_lasso_inference function
+#' @param title desired plot title
+#' @return ggplot2 object
+#' @import ggplot2
+#' @export
+plot_lasso_inference <- function(lasso_inference_table, title=NULL){
+  #plot pretty things
+  out.plot <-
+    ggplot(data = lasso_inference_table, aes(x = var_names, y = effect_size)) +
+    #geom_point(aes(color = var_names), cex = 4) +
+    geom_hline(aes(yintercept = 1), lty=2, color = 'black', alpha = 0.8) +
+    geom_errorbar(aes(ymin = lower, ymax=upper, color = var_names), lwd=0.8) +
+    geom_text(data= lasso_inference_table  %>% filter(significant==T),
+              aes(x=var_names, y=(upper*2), label='*'),
+              size=6)+
+    ggtitle(title) +
+    scale_color_discrete(guide=F) +
+    scale_y_continuous(trans = 'log') +
+    theme_tufte() +
+    coord_flip() +
+    xlab('Variables') +
+    ylab('Odds Ratio') +
+    theme(plot.title = element_text(size=16, hjust=0.5),
+          axis.title.y = element_blank(),
+          axis.title.x = element_text(size=14))
+  return(out.plot)
+}
 
 ###############################################################################################
